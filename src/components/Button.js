e5b5f0d484ccf109eee349dea7ac01981c1d2f6b
namrata shirsat <namrata.shirsaat@gmail.com>
2025-07-24 20:36:39 +0530

    change class to className

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,16 +2,16 @@ import styles from "./Button.module.css";
 
 
 function Button({ text, iconimg }) {
- return (
-  <button className={styles.button}>
-   <div class="svg-wrapper-1">
-    <div class="svg-wrapper d-flex justify-content-center">
-      <img src={iconimg} alt="icon" className={styles.iconImg} />
-     <span>{text}</span>
-    </div>
-   </div>
-  </button>
- );
+  return (
+    <button className={styles.button}>
+      <div className="svg-wrapper-1">
+        <div className="svg-wrapper d-flex justify-content-center">
+          <img src={iconimg} alt="icon" className={styles.iconImg} />
+          <span>{text}</span>
+        </div>
+      </div>
+    </button>
+  );
 }
 
 export default Button;
